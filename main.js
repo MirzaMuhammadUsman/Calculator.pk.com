df66425aafcaa7ae0cf55603a5ed805b0185a5c1
Muhammad Usman <109658635+MirzaMuhammadUsman@users.noreply.github.com>
2022-08-19 22:53:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function calc(b){
+var a= document.getElementById('inp')
+a.value += b    
+}
+function result(){
+    var gh = document.getElementById('inp')
+    gh.value = eval(gh.value)
+}
+
+// if (document.getElementById('btn')){
+//     var bn = prompt ('Do you want to exit?')
+// }
+
+// function abc(){
+// var b= prompt('Do you want to exit?')
+// }
+// if (b== no){
+//     document.write(gk)
+// }
+// var gk=('Thank you!')
+// var no =('no')
